0dd77417d02562161dda2c475e8295fba03c274b
Richard Walker <richard.walker.90@gmail.com>
2021-09-08 20:23:11 -0400

    Finish Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 import React from 'react';
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
 
   const fruits = [
@@ -9,13 +21,7 @@ const App = () => {
     { id: 3, name: 'Cherry', calories: 300, url: 'https://en.wikipedia.org/wiki/Cherry' }
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'Search For Fruit'
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'Fruit to search for');
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
